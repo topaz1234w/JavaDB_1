c8be5ef5c85a0bed5f5fa4c0305ebf93d15ef84a
Hanzo <topaz1234wwww@gmail.com>
2018-06-27 21:25:36 +0900

    1회차 if문 학점 예제

--- src/Day1/IF_EX.java
+++ src/Day1/IF_EX.java
@@ -16,12 +16,16 @@ public class IF_EX {
 		 * }
 		 * if와 else if의 순서가 중요!!!!
 		 */
+		
 		int num =0;
+		//만약 num가 0이면, 콘솔에 0입니다를 출력하세요.
 		if(num == 0){
 			System.out.println("0 입니다");
+		//만약 num를 2로 나누었을때 0과 같다면, 콘솔에 짝수입니다를 출력하세요.
 		}else if((num % 2) == 0){
 			System.out.println(num+"는 짝수 입니다.");
 		}
+		// 만약 num이 0이 아니고, num을 2로 나누었을 때 0과 같이 않다면 콜솔에 홀수를 출력하세요.
 		else{
 			System.out.println(num+"는 홀수 입니다.");
 		}
